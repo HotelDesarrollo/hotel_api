a313dad4df1026cd9f96efb12f8c2927f427da98
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2024-06-04 09:56:13 -0600

    mejoras

--- Hotel_api/settings.py
+++ Hotel_api/settings.py
@@ -49,7 +49,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic'
 ]
+
+LOCAL_APPS = [
+    'apps.alojamientos',
+    'apps.habitaciones',
+    'apps.usuarios',
+]
+
+THIRD_PARTY_APPS = [
+    'corsheaders',
+    'rest_framework',
+    'rest_framework_simplejwt',
+]
+
+INSTALLED_APPS = THIRD_PARTY_APPS + INSTALLED_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -141,4 +156,8 @@ if not DEBUG:
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+# Modelo de usuarios personalizado
+AUTH_USER_MODEL = 'usuarios.Usuario'
